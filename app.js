f06b157baa0e751d03ee4ecab45a6c63869c0fb3
damien.hervieu <damien.hervieu@ynov.com>
2017-06-01 16:06:48 +0200

    Changements EJS

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars');
 var expressValidator = require('express-validator');
 var flash = require('connect-flash');
 var session = require('express-session');
@@ -21,8 +20,7 @@ var app = express();
 
 //View Engine
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout:'layout'}));
-app.set('view engine', 'handlebars');
+app.set('view engine', 'ejs');
 
 //BodyParser and CookieParser Middleware
 app.use(bodyParser.json());
